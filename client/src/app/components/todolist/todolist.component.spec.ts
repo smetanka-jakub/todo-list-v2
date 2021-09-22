55f957b02db8ed6a3c15964eeecac94e2766d9e4
xsmetankaj <smetanka321@gmail.com>
2021-09-22 21:43:05 +0200

    add component Unit test for deleting todolist

--- client/src/app/components/todolist/todolist.component.spec.ts
+++ client/src/app/components/todolist/todolist.component.spec.ts
@@ -1,24 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog } from '@angular/material/dialog';
+import { of } from 'rxjs';
+import { TodoList } from 'src/app/shared/models/TodoList';
 
 import { TodolistComponent } from './todolist.component';
 
 describe('TodolistComponent', () => {
   let component: TodolistComponent;
-  let fixture: ComponentFixture<TodolistComponent>;
-
+  let todolists: TodoList[];
+  let dialog: MatDialog = {} as MatDialog;
+  let mockHeroService: any;
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [TodolistComponent]
-    }).compileComponents();
+    todolists = [
+      {
+        id: 10,
+        title: 'first todolist',
+        items: [
+          {
+            id: 68,
+            todolistId: 10,
+            title: 'sdad',
+            deadline: new Date('2021-06-22T22:00:00.000Z'),
+            description: 'asdads',
+            done: true
+          }
+        ],
+        query: '',
+        radio: ''
+      }
+    ];
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TodolistComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  mockHeroService = jasmine.createSpyObj(['getAllTodoLists', 'deleteTodoList']);
+  component = new TodolistComponent(dialog, mockHeroService);
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('delete', () => {
+    it('should remove indicated todolist from todolist list', () => {
+      mockHeroService.deleteTodoList.and.returnValue(of(true));
+      component.todoLists = todolists;
+      component.deleteTodoList(todolists[0]);
+
+      expect(component.todoLists.length).toBe(0);
+    });
   });
 });
